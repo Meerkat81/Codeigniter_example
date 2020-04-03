41cb18f9c9346d59d3b1de5313d640930c46d034
Casey <Cm>
2020-04-03 11:10:02 -0500

    added narrative and photo

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,1 +1,16 @@
-<p>Home Test</p>
+<div class="row">
+    <div class="col-lg-4">
+        <h1>Fox Body Mustang</h1>
+        <h2>American Classic</h2>
+    </div>
+    <div class="col-lg-4">
+        <img src="../../../assets/images/home.jpeg" class="img-fluid">
+    </div>
+    <div class="col-lg-4"
+        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+            aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+            occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+    </div>
+
+</div>
