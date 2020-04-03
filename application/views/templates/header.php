f5c067e6fceb9002b372260ae5b137c5baa6cd03
Casey <Cm>
2020-04-02 21:13:24 -0500

    working css link

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,6 +1,7 @@
 <html>
     <head>
         <title>CodeIgniter Tutorial</title>
+        <link href="../../../assets/css/main.css" type="text/css" rel="stylesheet" />
     </head>
     <body>
         <h1><?= $title?></h1>
