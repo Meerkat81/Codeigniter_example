85b39b9361d06d56f2ea042f851c4496c3a6d0a3
Casey <Cm>
2020-04-03 11:38:30 -0500

    added sticky footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -5,6 +5,10 @@ Purpose: footer template
 -->
 
 </div> <!-- closed container div -->
+    <div class="page-footer  fixed-bottom bg-dark">
+           <span>2020</span>
+    </div>
+    <!-- Copyright -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
         <script src="../../../assets/js/jquery-3.4.1.js"></script>
         <script src="../../../assets/js/bootstrap.min.js" ></script>
