bef09cb569954e196b23cfe878e84306edb388ee
Casey <Cm>
2020-04-02 22:41:12 -0500

    added nav

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -17,4 +17,20 @@ Purpose: Header template
         <link href="../../../assets/css/bootstrap.min.css" type="text/css" rel="stylesheet">
     </head>
     <body>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-3">
+        <div class="container-fluid">
+            <a href="#" class="navbar-brand mr-3">Fox-Body Mustang</a>
+            <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <div class="navbar-nav">
+                    <a href="#" class="nav-item nav-link active">Home</a>
+                    <a href="#" class="nav-item nav-link">Exterior</a>
+                    <a href="#" class="nav-item nav-link">Interior</a>
+                    <a href="#" class="nav-item nav-link">Specs</a>
+                </div>
+            </div>
+        </div>
+    </nav>
         <h1><?= $title?></h1>
