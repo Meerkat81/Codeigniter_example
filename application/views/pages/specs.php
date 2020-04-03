1c6b53e3969b0dcb125324b23d8df2ac0957801a
Casey <Cm>
2020-04-03 00:03:28 -0500

    added table and tested

---
+++ application/views/pages/specs.php
@@ -0,0 +1,58 @@
+<!--
+Name: Casey Meurer
+Coding 07
+Purpose: exterior horse power table
+-->
+<div>
+    <table class="table table-striped">
+        <thead>
+        <tr>
+            <th>Engine</th>
+            <th>Years</th>
+            <th>Horse Power</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>140-CID 4 Cyl</td>
+            <td>79 - 93</td>
+            <td>88 - 105</td>
+        </tr>
+        <tr>
+            <td>140 CID 4 Cyl Turbo</td>
+            <td>79 -  86</td>
+            <td>109 - 150</td>
+        </tr>
+        <tr>
+            <td>171 CID V-6</td>
+            <td>79 </td>
+            <td>109</td>
+        </tr>
+        <tr>
+            <td>200 CID I-6</td>
+            <td>79 </td>
+            <td>85 - 91</td>
+        </tr>
+        <tr>
+            <td>232 V-6</td>
+            <td>83 - 86</td>
+            <td>105 </td>
+        </tr>
+        <tr>
+            <td>255 CID V-8</td>
+            <td>80 - 82</td>
+            <td>119 </td>
+        </tr>
+        <tr>
+            <td>302 CID V-8</td>
+            <td>79 - 93</td>
+            <td>140 - 232</td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        </tbody>
+    </table>
+</div>
