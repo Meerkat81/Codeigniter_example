e43a3ba64baf7507ac411ebd817059467989ca55
Casey <Cm>
2020-04-03 11:11:20 -0500

    added gallery and tested

--- application/views/pages/interior.php
+++ application/views/pages/interior.php
@@ -1,2 +1,30 @@
-<p>About Test</p>
+<div class="row">
+    <div class="col-lg-4">
+        <img src="../../../assets/images/int0.jpg" class="img-fluid">
+    </div>
+    <div class="col-lg-4">
+        <img src="../../../assets/images/int1.jpg" class="img-fluid">
+    </div>
+    <div class="col-lg-4">
+        <img src="../../../assets/images/int2.jpg" class="img-fluid">
+    </div>
+    <div class="col-lg-4">
+        <img src="../../../assets/images/int3.jpg" class="img-fluid">
+    </div>
+    <div class="col-lg-4">
+        <img src="../../../assets/images/int4.jpeg" class="img-fluid">
+    </div>
+    <div class="col-lg-4">
+        <img src="../../../assets/images/int5.jpg" class="img-fluid">
+    </div>
+    <div class="col-lg-4">
+        <img src="../../../assets/images/int6.jpeg" class="img-fluid">
+    </div>
+    <div class="col-lg-4">
+        <img src="../../../assets/images/int7.jpeg" class="img-fluid">
+    </div>
+    <div class="col-lg-4">
+        <img src="../../../assets/images/int8.jpeg" class="img-fluid">
+    </div>
+</div>
 
