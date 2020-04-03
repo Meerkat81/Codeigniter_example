f156978df238f41a86866a57292f7f5da3e2cc8b
Casey <Cm>
2020-04-02 22:11:27 -0500

    added bootstrap links

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,3 +1,12 @@
-        <em>&copy; 2015</em>
+<!--
+Name: Casey Meurer
+Coding 07
+Purpose: footer template
+-->
+
+
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="../../../assets/js/jquery-3.4.1.js"></script>
+        <script src="../../../assets/js/bootstrap.min.js" ></script>
     </body>
 </html>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,7 +1,20 @@
+<!--
+Name: Casey Meurer
+Coding 07
+Purpose: Header template
+-->
+
 <html>
     <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+
         <title>CodeIgniter Tutorial</title>
         <link href="../../../assets/css/main.css" type="text/css" rel="stylesheet" />
+        <link href="../../../assets/css/bootstrap.min.css" type="text/css" rel="stylesheet">
     </head>
     <body>
         <h1><?= $title?></h1>
