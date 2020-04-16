330d7d270e690369a77cb6b5d2bfb55374aad3da
Casey <Cm>
2020-04-16 13:33:15 -0500

    changed name of conditionally loaded variable

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -12,5 +12,6 @@ Purpose: footer template
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
         <script src="assets/js/jquery-3.4.1.js"></script>
         <script src="assets/js/bootstrap.min.js" ></script>
+        <?= $contactjs; ?>
     </body>
 </html>
