f634b9be832b79b97f0052c902994c050a3353c7
Casey <Cm>
2020-05-12 11:03:02 -0500

    added formatting div and msg area

--- application/views/pages/API.php
+++ application/views/pages/API.php
@@ -1,1 +1,21 @@
-<?php
+<div class="row">
+    <div class="col-lg-12">
+        <div id="msg">
+            <!-- This is a blank area to talk with user -->
+            <br>
+        </div>
+        <form id="contact-data" name="emailForm" role="form" accept-charset="utf-8">
+            <div class="form-group row">
+                <div class="col-sm-10">
+                    <input name="vin" type="text" id="vin" class="form-control"  placeholder="Vehicle Identification Number or VIN" maxlength="64">
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-sm-10 offset-md-4">
+                    <button type="button" class="btn btn-primary align-self-center" id="send">Send</button>
+                    <button type="button" class="btn btn-primary align-self-center" id="clear">Clear</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
